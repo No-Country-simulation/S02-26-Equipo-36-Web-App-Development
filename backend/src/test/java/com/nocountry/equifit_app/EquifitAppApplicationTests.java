87d22199dc17281f0328e876155a8346e90cc887
Leandro Pollano <leandropollano@gmail.com>
2026-02-10 16:33:00 -0300

    init backend

---
+++ backend/src/test/java/com/nocountry/equifit_app/EquifitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.equifit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquifitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
